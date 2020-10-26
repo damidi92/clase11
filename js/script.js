6ac328c0f4010e8a5543cf9fbffe3359b37a2520
Damian Di Costanzo <dicostanzodamian@gmail.com>
2020-10-26 13:37:54 -0600

    Primer archivo del repositorio

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var pasta= prompt("Del 1 al 10,cuanto te gustan las pastas?");
+if (pasta>5) {
+	alert("Estas en el lugar correcto")
+}else if (pasta<=5) {
+	alert("Mira nuestros productos que te van a gustar")
+};
